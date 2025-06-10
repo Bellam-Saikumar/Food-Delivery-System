78b24d515e2422360869b66ed3219c4cfddc9be7
Bellam-Saikumar <139708278+Bellam-Saikumar@users.noreply.github.com>
2025-06-10 23:04:11 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-const url ='https://food-delivery-system-admin-t29r.onrender.com';
+const url ='https://food-delivery-system-admin-ah9g.onrender.com/';
   return (
     <div>
       <ToastContainer/>
